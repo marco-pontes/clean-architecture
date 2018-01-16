02e936ad76dfb66ab4574cf3aba188fd622f0fc5
Marco Aurelio Pontes <aurelio.pontes@gmail.com>
2018-01-16 14:48:03 -0200

    Changes

---
+++ src/main/java/interactors/FinancialReportGenerator.java
@@ -0,0 +1,8 @@
+package interactors;
+
+public class FinancialReportGenerator implements FinancialReportRequester {
+    @Override
+    public FinancialReportResponse getFinancialReport(FinancialReportRequest financialReportRequest) {
+        return null;
+    }
+}
